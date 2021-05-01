81ebbccfef9879964dfbe16d7bf9d630db4ee215
Alyona Gnevusheva <tchernovaalena@gmail.com>
2021-05-01 10:59:23 +0300

    добавлен тест на функционал парсинга

--- src/__tests__/parseInput.spec.js
+++ src/__tests__/parseInput.spec.js
@@ -1,26 +1,3 @@
-/* const CURRENCY = {
-  RUB: 'RUB',
-  USD: 'USD',
-  EUR: 'EUR',
-  GBP: 'GBP',
-};
-
-const parseInput = (input) => {
-  const indexes = {};
-  const upperCasedInput = input.toUpperCase();
-  Object.keys(CURRENCY).forEach((cur) => {
-    const idexOfCurInInput = upperCasedInput.indexOf(cur);
-    if (idexOfCurInInput !== -1) {
-      indexes[idexOfCurInInput] = cur;
-    }
-  });
-  const orderedIndexes = Object.keys(indexes);
-  const baseCur = indexes[orderedIndexes[0]];
-  const converseCur = indexes[orderedIndexes[1]];
-  const baseSum = parseInt(input, 10);
-  return { baseCur, converseCur, baseSum };
-}; */
-
 import parseInput from '../utils/parseInput';
 
 describe('Parse function', () => {
@@ -29,6 +6,16 @@ describe('Parse function', () => {
     const outputBaseCur = ('RUB');
     const outputConverseCur = ('EUR');
     const outputBaseSum = (30);
+    const { baseCur, converseCur, baseSum } = parseInput(input);
+    expect(outputBaseCur).toEqual(baseCur);
+    expect(outputConverseCur).toEqual(converseCur);
+    expect(outputBaseSum).toEqual(baseSum);
+  });
+  test('It shoul return two strings with currencies and one int', () => {
+    const input = '20 gbp RUB';
+    const outputBaseCur = ('GBP');
+    const outputConverseCur = ('RUB');
+    const outputBaseSum = (20);
     const { baseCur, converseCur, baseSum } = parseInput(input);
     expect(outputBaseCur).toEqual(baseCur);
     expect(outputConverseCur).toEqual(converseCur);
